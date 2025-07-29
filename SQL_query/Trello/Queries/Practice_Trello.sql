a33075594c08beaa23a78de0186fc6e599bce60e
Thong <quangthongsainto@gmail.com>
2025-07-29 08:32:40 +0700

    note Slide

--- SQL_query/Trello/Queries/Practice_Trello.sql
+++ SQL_query/Trello/Queries/Practice_Trello.sql
@@ -99,7 +99,7 @@ FROM Boards b
     JOIN OwnerTypes ot ON ot.Id = m.OwnerTypeId
     JOIN Workspaces w ON w.Id = b.WorkspaceId
     JOIN Users u ON u.Id = m.UserId
-WHERE ot.Value = 'Board' AND u.Id = 1 AND w.Id = 2
+WHERE ot.Value = 'Board' AND u.Id = 1 AND w.Id = 1
 
 -- Search boards by keyword in title. (Slide 10)
 SELECT b.[Name]
@@ -127,7 +127,7 @@ FROM ShareLinks sl
 WHERE w.Id = 9;
 
 -- Get all members of the board(id=1) with permissions (Slide 13)
-SELECT m.Id, u.Username,p.[Name]
+SELECT m.Id, u.PictureUrl, u.Username, u.Email ,p.[Name]
 FROM Members m
     JOIN Boards b ON b.Id = m.OwnerId
     JOIN Permissions p ON p.Id = m.PermissionId
@@ -141,7 +141,7 @@ FROM SettingKeys sk
 JOIN OwnerTypes ot ON ot.Id = sk.OwnerTypeId
 WHERE ot.[Value] = 'User' 
 
--- Get setting workspace(id=1001) visibility(value=22, user=200) (Slide 17)
+-- Get setting workspace(id=2) visibility(value=22, user=2) (Slide 17)
 SELECT w.[Name], sk.KeyName, so.DisplayValue
 FROM SettingKeys sk
 JOIN SettingValues sv ON sv.SettingKeyId = sk.Id
@@ -149,7 +149,7 @@ JOIN OwnerTypes ot ON ot.Id = sk.OwnerTypeId
 JOIN SettingOptions so ON so.Id = sv.Value
 JOIN Workspaces w ON w.Id = sv.OwnerId
 JOIN Users u ON u.Id = w.CreatedBy
-WHERE ot.[Value] = 'Workspace' AND sk.KeyName = 'workspacevisibility' AND u.Id = 200 AND  w.Id = 1001;
+WHERE ot.[Value] = 'Workspace' AND sk.KeyName = 'workspacevisibility' AND u.Id = 2 AND  w.Id = 2;
 
 -- Get all setting keys of user (id=1) with option value (Slide 18)
 SELECT sk.KeyName, sv.[Value] AS SettingValueId, so.DisplayValue AS SettingOption, u.Id
@@ -158,7 +158,7 @@ JOIN SettingValues sv ON sv.SettingKeyId = sk.Id
 JOIN OwnerTypes ot ON sk.OwnerTypeId = ot.Id
 JOIN SettingOptions so ON so.Id = sv.[Value]
 JOIN Users u ON u.Id = sv.OwnerId 
-WHERE ot.[Value] = 'User' AND  u.Id = 1
+WHERE ot.[Value] = 'User' AND  u.Id = 6
 ORDER BY sk.KeyName;
 
 -- Get SettingKeys and SettingOptions of this SettingKeys  for Workspace (Slide 17)
@@ -178,7 +178,7 @@ JOIN BoardPowerUps bu ON bu.PowerUpId = pu.Id
 JOIN Boards b ON b.Id = bu.BoardId
 JOIN PowerUpCategories puc ON pu.PowerUpCategoryId = puc.Id
 JOIN Workspaces w ON w.Id = b.Id
-WHERE w.Id = 2 AND pu.IsStaffPick = 1;
+WHERE w.Id = 1 AND pu.IsStaffPick = 1;
 
 -- Get detail information power up(category=2) (Slide 22)
 SELECT pu.Name, pu.[Description], pu.IconUrl, pu.AuthorName, pu.BackgroundUrl, puc.[Name]
@@ -253,12 +253,12 @@ WHERE s.Position= 1 AND b.Id = 3;
 
 
 -- Get members in card(id=1) (Slide 33)
-SELECT u.Id, u.PictureUrl, c.Id
+SELECT u.Id, u.PictureUrl
 FROM Members m
 JOIN OwnerTypes ot ON ot.Id = m.OwnerTypeId
 JOIN Cards c ON c.Id = m.OwnerId
 JOIN Users u ON u.Id = m.UserId
-WHERE ot.Value = 'Card' AND c.Id = 4;
+WHERE ot.Value = 'Card' AND c.Id = 1;
 
 -- Get information card (id=1) of a stage(id=2), board(id=2) (Slide 34)
 WITH CountAttachmentCard AS (
@@ -285,15 +285,13 @@ WHERE s.Id = 2 AND b.Id = 2;
 --JOIN Members m ON m.OwnerId = c.Id
 --JOIN OwnerTypes ot ON ot.Id = m.OwnerTypeId 
 
--- Get labels of card(id=39) (Slide 35)
+-- Get labels of card(id=720) (Slide 35)
 SELECT c.Id, c.Title, col.[Name], col.Icon
 FROM Labels l
 JOIN CardLabels cl ON cl.LabelId = l.Id
 JOIN Cards c ON c.Id = cl.CardId
 JOIN Colors col ON col.Id = l.ColorId
-JOIN Stages s ON s.Id =c.StageId
-JOIN Boards b ON b.Id = s.BoardId
-WHERE b.Id = 1 AND s.Id = 1 AND c.Id = 39;
+WHERE c.Id = 1;
 
 -- Get activities of card(id=12) (Slide 36)
 SELECT a.[Description], a.CreatedAt, c.id, u.Username, u.PictureUrl
@@ -301,15 +299,15 @@ FROM Activities a
 JOIN OwnerTypes ot ON ot.Id = a.OwnerTypeId
 JOIN Cards c ON c.Id = a.OwnerId
 JOIN Users u ON u.Id = a.UserId
-WHERE ot.Value = 'Card' AND c.Id = 12
+WHERE ot.Value = 'Card' AND c.Id = 1
 ORDER BY a.CreatedAt DESC
 
--- Get list Photo Uplash of card(id=12) (Slide 37)
+-- Get list Photo Uplash of card(id=189) (Slide 37)
 SELECT c.CoverValue
 FROM Cards c
 JOIN Stages s ON s.Id = c.StageId
 JOIN Boards b ON b.Id = s.BoardId
-WHERE c.CoverType = 'UNSPLASH' AND b.Id = 11 AND c.Id = 2 AND c.Position = 2
+WHERE c.CoverType = 'UNSPLASH' AND b.Id = 1 AND c.Id = 189 AND c.Position = 4
 
 -- Get list color of card(id=12) (Slide 37)
 SELECT c.[Name], c.Icon
@@ -324,8 +322,8 @@ JOIN Cards c ON c.Id = cl.CardId
 WHERE c.Id = 1
 
 
--- Get list all checklist items assigned to the user with status(= false). (Slide 40)
-SELECT cl.[Name] AS ChecklistName, cli.[Name] AS ChecklistItemName , cli.[Status] , ca.Id AS CardId
+-- Get list all checklist items assigned to the user. (Slide 40)
+SELECT cl.[Name] AS ChecklistName, cli.[Name] AS ChecklistItemName ,cli.Position, cli.[Status] , ca.Id AS CardId
 FROM CheckListItems cli
 JOIN CheckLists cl ON cl.Id = cli.CheckListId
 JOIN Cards ca ON ca.Id = cl.CardId
@@ -333,13 +331,13 @@ JOIN Stages st ON st.Id = ca.StageId
 JOIN Boards bo ON bo.Id = st.BoardId
 JOIN Members me ON me.Id = cli.MemberId
 JOIN Users us ON us.Id = me.UserId
-WHERE cli.[Status] = 0 AND ca.Id = 832
+WHERE  ca.Id = 91
 
 -- Get attachments of card (id = 1) (Slide 42)
 SELECT a.[Name], a.Link ,a.UploadAt, a.IsCover
 FROM Attachments a
 JOIN Cards c ON c.Id = a.CardId
-WHERE c.Id = 42 
+WHERE c.Id = 50
 ORDER BY UploadAt DESC
 
 -- Get comment of card (id = 1) (Slide 43)
@@ -349,23 +347,67 @@ JOIN Cards ca ON ca.Id = c.CardId
 JOIN Stages s ON s.Id = ca.StageId
 JOIN Boards b ON b.Id = s.BoardId 
 JOIN Users u ON u.Id = c.CreatedBy
-WHERE ca.Id = 1 AND s.Id = 35 AND b.Id = 35
+WHERE ca.Id = 1 AND s.Id = 159 AND b.Id = 159
 
--- Get reaction of comment's card (id = 201) (Slide 43)
-SELECT r.Id, r.Icon
+-- Get reaction of comment's card (id = 201) (Slide 43) 
+SELECT r.Id, r.Icon, COUNT(*) AS NumberReaction
 FROM Reactions r
 JOIN CommentReactions cr ON cr.ReactionId = r.Id
 JOIN Comments c ON c.Id = cr.CommentId
-JOIN Cards ca ON ca.Id = c.CardId
-WHERE ca.Id = 201 
+WHERE c.Id = 104
+GROUP BY r.Id, r.Icon;
 
--- Get reaction of comment's card (id = 201) (Slide 43)
-SELECT r.Id, r.Icon
-FROM Reactions r
-JOIN CommentReactions cr ON cr.ReactionId = r.Id
-JOIN Comments c ON c.Id = cr.CommentId
-JOIN Cards ca ON ca.Id = c.CardId
-WHERE ca.Id = 201 
+--  Show all CustomField and Selection of a specific card (Slide 45)
+WITH CustomFieldOfCard AS (
+    SELECT 
+        Id,
+        Title,
+        FieldType,
+        Position,
+        BoardId
+    FROM CustomFields cf
+    WHERE BoardId = (
+        SELECT 
+            st.BoardId
+        FROM (
+            SELECT 
+                Id,
+                StageId
+            FROM Cards
+            WHERE Id = 1
+        ) ca
+        JOIN Stages st ON st.Id = ca.StageId
+    )
+)
+SELECT
+    cfoc.Id AS custom_field_id,
+    cfoc.Title AS custom_field_title,
+    cfoc.FieldType AS custom_field_type,
+    cfoc.Position AS custom_field_position,
+    fv.[Value] AS field_value,
+    CASE 
+        WHEN cfoc.FieldType = 'DROPDOWN' THEN fi.[Value]
+        ELSE fv.[Value] 
+    END AS field_item_value,
+    fv.CardId AS card_id
+FROM CustomFieldOfCard cfoc
+JOIN FieldValues fv ON fv.CustomFieldId = cfoc.Id AND fv.CardId = 1
+LEFT JOIN FieldItems fi ON cfoc.FieldType = 'DROPDOWN' AND fi.Id = TRY_CAST(fv.[Value] AS INT);
+
+-- Show all options of a custom field(id=13) with DROPDOWN type (Slide 45)
+SELECT 
+    cf.Id AS custom_field_id,
+    cf.Title AS custom_field_title,
+    cf.FieldType AS custom_field_type,
+    fi.[Value] AS field_item_value,
+    fi.[Priority] AS field_item_priority,
+    co.[Name] AS color_name,
+    co.Icon AS color_icon
+FROM CustomFields cf
+JOIN FieldItems fi ON fi.CustomFieldId = cf.Id
+JOIN Colors co ON co.Id = fi.ColorId
+WHERE cf.Id = 13
+ORDER BY fi.[Priority];
 
 -- Get list sticker  (Slide 47)
 SELECT s.Id, s.[Name], s.StickerUrl
@@ -378,15 +420,35 @@ JOIN CardStickers cs ON cs.StickerId = s.Id
 JOIN Cards c ON c.Id = cs.CardId
 WHERE c.Id = 1
 
+-- Get list all recent card's activities in the user's card. (Slide 49)
+SELECT ca.Title , wo.[Name] AS WorkSpace , bo.[Name]  AS Board, st.Title , us.Username , us.PictureUrl , ac.[Description],
+       ABS(DATEDIFF(DAY, GETDATE(), ac.CreatedAt)) AS day_ago
+FROM (SELECT UserId, OwnerTypeId, OwnerId, [Description], CreatedAt
+    FROM Activities WHERE OwnerTypeId = 3) ac
+JOIN Cards ca ON ca.Id = ac.OwnerId AND ca.CreatedBy = 1
+JOIN Stages st ON st.Id = ca.StageId
+JOIN Boards bo ON bo.Id = st.BoardId
+JOIN Workspaces wo ON wo.Id = bo.WorkspaceId
+JOIN Users us ON us.Id = ac.UserId
+Order By day_ago;
+
 -- Show Activities unread notifications of the user. (Slide 50)
 SELECT u.Username, u.PictureUrl, a.[Description], a.CreatedAt
 FROM Activities a
 JOIN Notifications n ON a.Id = n.ActivityId
 JOIN  Users u ON u.Id = a.UserId
-WHERE u.Id = 2 AND n.Status = 'UNREAD'
+WHERE u.Id = 2 AND n.[Status] = 'UNREAD'
+
+-- Count total number of unread notifications of the user. (Slide 50)
+SELECT COUNT(ac.UserId) AS NumberOfNotifications
+FROM Notifications n
+JOIN Activities ac ON ac.Id = n.ActivityId
+WHERE ac.UserId = 2 AND n.[Status] = 'UNREAD';
 
 -- Get list boards in collection (id = 1) (Slide 52)
-SELECT b.[Name], b.BackgroundUrl
-FROM Boards b
-JOIN BoardCollections bc ON bc.BoardId = b.Id
+SELECT b.[Name] ,co.[Name] 
+FROM BoardCollections bc
+JOIN Boards b ON b.Id = bc.BoardId
+JOIN Collections co ON co.Id = bc.CollectionId
+WHERE b.WorkspaceId = 1
 
