e2e7f4af7ced9d5c095aebc4825fe15c4f425cfb
Thong <quangthongsainto@gmail.com>
2025-07-29 08:36:22 +0700

    adjust data file

--- SQL_query/Trello/InsertSeedData/OwnerTypes.sql
+++
@@ -1,4 +0,0 @@
-insert into OwnerTypes (Value) values ('Workspace')
-insert into OwnerTypes (Value) values ('Board');
-insert into OwnerTypes (Value) values ('Card');
-insert into OwnerTypes (Value) values ('User');
